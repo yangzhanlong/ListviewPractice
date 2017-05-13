27c518c3a787d619d196020c44109dfa8fdf849e
Zhanlong Yang <673029686@qq.com>
2017-05-13 17:01:27 +0800

    Use ArrayAdapter with listview

--- app/src/main/java/com/example/user/listviewpractice/MainActivity.java
+++ app/src/main/java/com/example/user/listviewpractice/MainActivity.java
@@ -1,17 +1,26 @@
 package com.example.user.listviewpractice;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
+    String[] list = new String[]{"test1", "test2", "test3", "test4"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         ListView listView = (ListView) findViewById(R.id.lv);
-        listView.setAdapter(new MyAdapter(this));
+
+        // BaseAdapter
+        //listView.setAdapter(new MyAdapter(this));
+
+        // ArrayAdapter
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.item1, list);
+        listView.setAdapter(adapter);
+
     }
 }
--- app/src/main/java/com/example/user/listviewpractice/MyAdapter.java
+++ app/src/main/java/com/example/user/listviewpractice/MyAdapter.java
@@ -32,7 +32,18 @@ public class MyAdapter extends BaseAdapter{
     public View getView(int position, View convertView, ViewGroup parent) {
         View view;
         if (convertView == null) {
+            //创建新的view对象   可以通过打气筒把一个布局资源转换成一个view对象
+            //resource 就是 我们定义的布局文件
+
+            //[一☆☆☆☆]获取打气筒服务
             view = View.inflate(mainActivity.getApplicationContext(), R.layout.item, null);
+
+            //[二☆☆☆☆]获取打气筒服务
+            //view = LayoutInflater.from(mainActivity.getApplicationContext()).inflate(R.layout.item, null);
+
+            //[三☆☆☆☆]获取打气筒服务
+            //LayoutInflater layoutInflater = (LayoutInflater) mainActivity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            //layoutInflater.inflate(R.layout.item, null);
         } else {
             view = convertView;
         }
