e49ff421d7142c80c05b4e1c96d3f622c6fe5fad
Zhanlong Yang <673029686@qq.com>
2017-05-13 17:45:24 +0800

    Use SimpleAdapter with listview

--- app/src/main/java/com/example/user/listviewpractice/MainActivity.java
+++ app/src/main/java/com/example/user/listviewpractice/MainActivity.java
@@ -2,8 +2,13 @@ package com.example.user.listviewpractice;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,8 +24,31 @@ public class MainActivity extends AppCompatActivity {
         //listView.setAdapter(new MyAdapter(this));
 
         // ArrayAdapter
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.item1, list);
-        listView.setAdapter(adapter);
+        //ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.item1, list);
+        //listView.setAdapter(adapter);
 
+
+        // SimpleAdapter
+        List<Map<String, String>> list = new ArrayList<>();
+        Map<String, String> map1 = new HashMap<>();
+        map1.put("name", "test");
+        map1.put("phone", "123");
+
+        Map<String, String> map2 = new HashMap<>();
+        map2.put("name", "test2");
+        map2.put("phone", "456");
+
+        Map<String, String> map3 = new HashMap<>();
+        map3.put("name", "test3");
+        map3.put("phone", "789");
+
+        list.add(map1);
+        list.add(map2);
+        list.add(map3);
+
+        SimpleAdapter simpleAdapter = new SimpleAdapter(this, list, R.layout.item2,
+                new String[]{"name", "phone"}, new int[] {R.id.name, R.id.phone});
+
+        listView.setAdapter(simpleAdapter);
     }
 }
